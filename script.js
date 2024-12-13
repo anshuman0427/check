0f714d019efb569918f7f7a8330844a008dfef1a
Anshuman Mishra <anshuman0427@gmail.com>
2024-12-13 05:55:30 +0530

    privacy policy included

--- script.js
+++ script.js
@@ -40,14 +40,14 @@ video.addEventListener("play", () => {
       width: video.width,
     });
 
-    faceapi.draw.drawDetections(canvas, resizedWindow, {FillStyle: "#D2FFF0FF"});
-    faceapi.draw.drawFaceLandmarks(canvas, resizedWindow, {FillStyle: "#68FF5AFF"});
-    faceapi.draw.drawFaceExpressions(canvas, resizedWindow, {FillStyle: "#F33B3BFF"});
+    faceapi.draw.drawDetections(canvas, resizedWindow);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedWindow);
+    faceapi.draw.drawFaceExpressions(canvas, resizedWindow);
 
     resizedWindow.forEach((detection) => {
       const box = detection.detection.box;
       const drawBox = new faceapi.draw.DrawBox(box, {
-        label: Math.round(detection.age) + " year old " + detection.gender,
+        label: Math.round(detection.age) + " years old " + detection.gender,
         boxColor: "#0279F9FF", // Set detection box color
     lineWidth: 3,
       });
